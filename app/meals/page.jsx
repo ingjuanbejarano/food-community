91528a4735b49c047c842e1925ffbd945d02d645
Juan Carlos Bejarano <jcbj1292@gmail.com>
2025-01-28 18:21:13 -0500

    meal page and dummy databse

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,19 +1,29 @@
-import Link from 'next/link';
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getAllMeals } from "@/lib/meals";
 
-const MealsPage = () => {
-	return (
-		<main>
-			<h1>Meals</h1>
-			<ul>
-				<li>
-					<Link href="meals/pizza">Pizza</Link>
-				</li>
-				<li>
-					<Link href="meals/burger">Burger</Link>
-				</li>
-			</ul>
-		</main>
-	);
+const MealsPage = async () => {
+  const meals = await getAllMeals()
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Tasty meals, all created{" "}
+          <span className={classes.highlight}>By you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself!, it is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your own recipe!</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
