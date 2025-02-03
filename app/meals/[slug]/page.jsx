52d02cab237fa8f24491b2dadb4b52ef6afb2ce0
Juan Carlos Bejarano <jcbj1292@gmail.com>
2025-02-03 15:51:02 -0500

    metadata for dynamic meals

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -3,6 +3,19 @@ import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
+export async function generateMetadata({ params }) {
+	const meal = getMeal(params.slug);
+
+	if (!meal) {
+		notFound();
+	}
+
+	return {
+		title: meal.title,
+		description: meal.summary,
+	};
+}
+
 const MealPage = async ({ params }) => {
   const { slug } = await params;
   const meal = getMeal(slug);
